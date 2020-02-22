42a5473da6d845f25e4e59ca1f094561ef015685
akimovaen <akimova.lena.n@gmail.com>
2020-02-22 18:38:58 +0200

    Create views for app

--- management/models.py
+++ management/models.py
@@ -18,24 +18,35 @@ class Staff(models.Model):
 
 
 class Schedule(models.Model):
-    shop = models.ForeignKey('Shops', on_delete=models.CASCADE,)
-    staff_member = models.ForeignKey('Staff', on_delete=models.CASCADE,)
+    shop = models.ForeignKey('Shop', on_delete=models.CASCADE,)
+    staff_member = models.ForeignKey('Staff', on_delete=models.CASCADE, related_name='works')
     date = models.DateField('working day')
     hours = models.PositiveIntegerField()
 
 
-class Shops(models.Model):
+class Shop(models.Model):
+    SW = 'SW'
+    SS = 'SS'
+    TRADEMARK = [(SW, 'SW'), (SS, 'SS')]
     name = models.CharField(max_length=20)
+    trademark = models.CharField(max_length=2, choices=TRADEMARK)
+    adress = models.CharField(max_length=128, blank=True)
+    email = models.EmailField(blank=True, null=True)
 
 
-class Costs(models.Model):
-    name = models.CharField(max_length=20)
+class Cost(models.Model):
+    name = models.CharField(max_length=128)
+    cost_type = models.ForeignKey('Cost_type', on_delete=models.CASCADE,)
+
+
+class Cost_type(models.Model):
+    name = models.CharField(max_length=50)
 
 
 class ActivityLog(models.Model):
     date = models.DateField('day')
-    shop = models.ForeignKey('Shops', on_delete=models.CASCADE,)
-    cost = models.ForeignKey('Costs', on_delete=models.CASCADE,)
-    amount = models.DecimalField(max_digits=7, decimal_places=2)
+    shop = models.ForeignKey('Shop', on_delete=models.CASCADE, related_name='acts')
+    cost = models.ForeignKey('Cost', on_delete=models.CASCADE,)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
     staff_member = models.ForeignKey('Staff', on_delete=models.CASCADE,)
-    comment = models.CharField(max_length=50)
+    comment = models.CharField(max_length=50, blank=True)
--- management/views.py
+++ management/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from . import models
 
 
 # Create your views here.
 
+class ShopListView(ListView):
+    template_name = 'shop_list.html'
+    queryset = models.Shop.objects.all()
 
-def index(request):
-    return HttpResponse('Welcome to Management')
 
+class ShopDetailView(DetailView):
+    template_name='shop_month.html'
+    model = models.Shop
+
+
+class StaffListView(ListView):
+    template_name = 'staff.html'
+    queryset = models.Staff.objects.all()
+
+
+class StaffDetailView(DetailView):
+    template_name='person.html'
+    model = models.Staff
